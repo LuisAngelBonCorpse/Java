a4edce0a7150b3d764ab126878f55a753f33b0c5
Luis Angel Duran Lopez <luisadl591@gmail.com>
2021-10-06 18:03:53 -0500

    hola mundo java

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {  
+
+  public static void main(String[] args){
+    System.out.println("Holiwis mundo de java, yo Luis Angel aqui de nuevo y esta vez te vere a fondo");
+  }
+
+}
